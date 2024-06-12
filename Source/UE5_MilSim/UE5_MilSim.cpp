a4f8524697678ae2c227fb1706080be6fbe76075
Matt Hunter <mhunter7@uclan.ac.uk>
2024-06-12 19:12:56 +0100

    Initial commit

---
+++ Source/UE5_MilSim/UE5_MilSim.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_MilSim.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_MilSim, "UE5_MilSim" );
---
+++ Source/UE5_MilSim/UE5_MilSimGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_MilSimGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_MILSIM_API AUE5_MilSimGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
